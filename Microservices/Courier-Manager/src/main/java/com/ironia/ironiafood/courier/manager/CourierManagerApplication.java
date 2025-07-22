6f2c1879c2dc629a8861bb907d1f1cedddbdad67
r41l1m4 <rai.lima17@gmail.com>
2025-07-22 13:56:45 -0300

    Initial commit

---
+++ Microservices/Courier-Manager/src/main/java/com/ironia/ironiafood/courier/manager/CourierManagerApplication.java
@@ -0,0 +1,13 @@
+package com.ironia.ironiafood.courier.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierManagerApplication.class, args);
+	}
+
+}
---
+++ Microservices/Courier-Manager/src/test/java/com/ironia/ironiafood/courier/manager/CourierManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironia.ironiafood.courier.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
