72120394a5fbe9689b9e783b7fb99c186d7d4447
r41l1m4 <rai.lima17@gmail.com>
2025-07-29 14:11:23 -0300

    delivery_tracker - add domain models

---
+++ Microservices/Delivery-Tracker/src/main/java/com/ironia/ironiafood/delivery/tracker/domain/exception/DomainException.java
@@ -0,0 +1,14 @@
+package com.ironia.ironiafood.delivery.tracker.domain.exception;
+
+public class DomainException extends RuntimeException {
+    public DomainException() {
+    }
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+    public DomainException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ Microservices/Delivery-Tracker/src/main/java/com/ironia/ironiafood/delivery/tracker/domain/model/ContactPoint.java
@@ -0,0 +1,19 @@
+package com.ironia.ironiafood.delivery.tracker.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@Getter
+public class ContactPoint {
+    private String zipCode;
+    private String street;
+    private String number;
+    private String complement;
+    private String name;
+    private String phone;
+}
