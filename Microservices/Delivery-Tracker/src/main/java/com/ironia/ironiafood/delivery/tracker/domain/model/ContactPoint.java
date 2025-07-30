e730da094e596f4ab161612c87b316f652c12d58
r41l1m4 <rai.lima17@gmail.com>
2025-07-30 10:18:11 -0300

    delivery_tracker - add persistence conf

--- Microservices/Delivery-Tracker/src/main/java/com/ironia/ironiafood/delivery/tracker/domain/model/ContactPoint.java
+++ Microservices/Delivery-Tracker/src/main/java/com/ironia/ironiafood/delivery/tracker/domain/model/ContactPoint.java
@@ -1,14 +1,14 @@
 package com.ironia.ironiafood.delivery.tracker.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import jakarta.persistence.Embeddable;
+import lombok.*;
 
+@Embeddable
 @AllArgsConstructor
 @Builder
 @EqualsAndHashCode
 @Getter
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 public class ContactPoint {
     private String zipCode;
     private String street;
