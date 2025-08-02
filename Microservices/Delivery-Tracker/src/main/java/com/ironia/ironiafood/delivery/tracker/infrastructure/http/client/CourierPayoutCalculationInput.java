f8f35f5355f7b937ab9a37c0b72ed97e1779ce5e
r41l1m4 <rai.lima17@gmail.com>
2025-08-02 10:49:36 -0300

    delivery_tracker - add courier payout integration

---
+++ Microservices/Delivery-Tracker/src/main/java/com/ironia/ironiafood/delivery/tracker/infrastructure/http/client/CourierPayoutCalculationInput.java
@@ -0,0 +1,13 @@
+package com.ironia.ironiafood.delivery.tracker.infrastructure.http.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CourierPayoutCalculationInput {
+
+    private Double distanceInKm;
+}
---
+++ Microservices/Delivery-Tracker/src/main/java/com/ironia/ironiafood/delivery/tracker/infrastructure/http/client/CourierPayoutResultModel.java
@@ -0,0 +1,15 @@
+package com.ironia.ironiafood.delivery.tracker.infrastructure.http.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class CourierPayoutResultModel {
+
+    private BigDecimal payoutFee;
+}
