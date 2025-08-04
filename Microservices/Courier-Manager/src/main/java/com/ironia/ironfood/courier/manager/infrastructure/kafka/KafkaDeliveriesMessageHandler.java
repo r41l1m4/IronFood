066bc88352096694b53df32b05e09512d63de267
r41l1m4 <rai.lima17@gmail.com>
2025-08-04 15:11:15 -0300

    courier_manager - impl integration event listener

---
+++ Microservices/Courier-Manager/src/main/java/com/ironia/ironfood/courier/manager/infrastructure/kafka/KafkaDeliveriesMessageHandler.java
@@ -0,0 +1,32 @@
+package com.ironia.ironfood.courier.manager.infrastructure.kafka;
+
+import com.ironia.ironfood.courier.manager.infrastructure.event.DeliveryFulfilledIntegrationEvent;
+import com.ironia.ironfood.courier.manager.infrastructure.event.DeliveryPlacedIntegrationEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@KafkaListener(topics = {"${kafka.topics.delivery.name}"}, groupId = "${spring.application.name}")
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaDeliveriesMessageHandler {
+
+    @KafkaHandler(isDefault = true)
+    public void defaultHandler(@Payload Object payload) {
+        log.info("Default handler: {}" , payload);
+    }
+
+    @KafkaHandler
+    public void handle(@Payload DeliveryPlacedIntegrationEvent event) {
+        log.info("Received: {}", event);
+    }
+
+    @KafkaHandler
+    public void handle(@Payload DeliveryFulfilledIntegrationEvent event) {
+        log.info("Received: {}", event);
+    }
+}
